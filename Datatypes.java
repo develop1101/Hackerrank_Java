d31d7b98af5d687db39ef862836dcd490a7394ba
develop1101 <43621205+develop1101@users.noreply.github.com>
2018-11-14 15:42:32 -0500

    Update Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -62,11 +62,57 @@ for the primitive data types discussed in this problem.
   byte      1 byte             -128 to 127
   short     2 byte             -32,768 to 32,767
   int       4 byte             -2,147,483,648 to 2,147,483, 647
-  lont      8 byte             -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+  long      8 byte             -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
 -----------------------------------------------
 if you recognize the positive number of the primitive variable is alway less with 1, 
 check the Byte the minimum  is -128 and the positive is not 128, its 128 -1 = 127, 
 so we add -1 so we can have the right positive range and have the right value.
 
+In this way, you can use Math.Pow(2, n) to solve this problem.
+For example, the minimum of short is -2^15, which is -32,768, and the maximum of short is 2^15 - 1, which is 32,767.
+So, inside the if statement for type short,
+-------------------------------------------
+if ( x >= -(Math.pow(2,15)) && x <= (Math.pow(2,15)-1))
+    System.out.println("* short");
+-------------------------------------------
+
+However, I am going to use MIN_VALUE and MAX_VALUE functions here.
+
 *****************************************************************************************/
 
+import java.util.*;
+import java.io.*;
+
+
+
+public class Solution{
+    public static void main(String []argh)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
+                if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)
+                  System.out.println("* Short");
+                if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)
+                  System.out.println("* Integer");
+                if(x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)
+                  System.out.println("* Long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
